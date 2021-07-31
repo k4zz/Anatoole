661d1d6385bf7c648bf5ff018ac21a0c49419017
Maciej Kazmierczak <m.kazmierczak93@gmail.com>
2021-07-31 11:22:49 +0200

    Removed version file added by mistake, added verison.h to ignore list

--- app/include/version.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-constexpr const char* PROJECT_NAME = "Anatoole";
-constexpr const char* PROJECT_VERSION = "0.1";
-constexpr const char* PROJECT_VERSION_MAJOR = "0";
-constexpr const char* PROJECT_VERSION_MINOR = "1";
