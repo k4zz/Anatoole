5783645cc13ac23e2f7ef5f3968fecf2473892c7
Maciej Kazmierczak <m.kazmierczak93@gmail.com>
2021-08-01 23:36:04 +0200

    Added first version of GUI elements

--- gui/src/MainWindow.cpp
+++ gui/src/MainWindow.cpp
@@ -2,6 +2,12 @@
 
 #include "ui_MainWindow.h"
 
+#include "PathsWidget.h"
+#include "LogConsoleWidget.h"
+#include "SettingsWidget.h"
+
+#include <QPushButton>
+
 MainWindow::MainWindow(QWidget* _parent)
         : QMainWindow(_parent)
         , ui(new Ui::MainWindow)
@@ -10,4 +16,13 @@ MainWindow::MainWindow(QWidget* _parent)
     auto windowTitle = QString(QCoreApplication::applicationName() + " v" + QCoreApplication::applicationVersion());
     setWindowTitle(windowTitle);
 
+    auto* pathsWidget = new PathsWidget(this);
+    auto* settingsWidget = new SettingsWidget(this);
+    ui->leftPanel->addWidget(pathsWidget);
+    ui->leftPanel->addWidget(new QPushButton("Analizuj", this));
+    ui->leftPanel->addWidget(settingsWidget);
+    ui->leftPanel->addStretch();
+
+    auto* widget2 = new LogConsoleWidget(this);
+    ui->rightPanel->addWidget(widget2);
 }
