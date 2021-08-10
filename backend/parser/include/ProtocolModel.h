db883395fb727d71510ee0eb6f2bd0843892d128
Maciej Kazmierczak <m.kazmierczak93@gmail.com>
2021-08-10 12:15:35 +0200

    Added Parser class

---
+++ backend/parser/include/ProtocolModel.h
@@ -0,0 +1,14 @@
+#pragma once
+
+struct Entry
+{
+    int row;
+    std::string key;
+    std::string value;
+};
+
+class ProtocolModel
+{
+    ProtocolModel()
+    
+};
---
+++ backend/parser/src/Parser.cpp
@@ -0,0 +1,8 @@
+#include "Parser.h"
+
+Parser::Parser(std::string _path)
+        : path(_path)
+{
+
+}
+
