1c7de0c88915ce99256af517862adb3d4bfc1bbf
Maciej Kazmierczak <m.kazmierczak93@gmail.com>
2021-08-14 22:23:13 +0200

    Added extended info to logs about filename and line number

--- core/logger/src/Logger.cpp
+++ core/logger/src/Logger.cpp
@@ -8,10 +8,10 @@ Logger& Logger::instance()
     return instance;
 }
 
-void Logger::log(const std::string& _msg, LogLevel _level)
+void Logger::log(const std::string& _msg, LogLevel _level, const std::string& _fileName, int _line)
 {
-    std::cout << toString(_level) << "\t" << _msg << "\n";
-    notifySubscribers(_msg, _level);
+    std::cout << _fileName << ":" << _line << "\t" << toString(_level) << "\t" << _msg << "\n";
+    notifySubscribers(_msg, _level, _fileName, _line);
 }
 
 void Logger::subscribe(ILoggerObserver* _observer)
@@ -24,10 +24,10 @@ void Logger::unsubscribe(ILoggerObserver* _observer)
     subscribers.remove(_observer);
 }
 
-void Logger::notifySubscribers(const std::string& _msg, LogLevel _level)
+void Logger::notifySubscribers(const std::string& _msg, LogLevel _level, const std::string& _fileName, int _line)
 {
     for(auto subscriber : subscribers)
     {
-        subscriber->log(_msg, _level);
+        subscriber->log(_msg, _level, _fileName, _line);
     }
 }
--- gui/src/LogConsoleWidget.cpp
+++ gui/src/LogConsoleWidget.cpp
@@ -10,13 +10,15 @@ LogConsoleWidget::LogConsoleWidget(QWidget* _parent)
 
     Logger::instance().subscribe(this);
 
-    ui->tbLogConsole->setHorizontalHeaderLabels(QStringList{"Level", "Message"});
+    ui->tbLogConsole->setHorizontalHeaderLabels(QStringList{"Level", "Place", "Message"});
+    ui->tbLogConsole->setColumnWidth(0,80);
+    ui->tbLogConsole->setColumnWidth(1,150);
     ui->tbLogConsole->horizontalHeader()->setSectionResizeMode(QHeaderView::Fixed);
 
     connect(ui->btnClear, &QPushButton::clicked, this, &LogConsoleWidget::onClearButtonClicked);
 }
 
-void LogConsoleWidget::log(const std::string& _msg, LogLevel _level)
+void LogConsoleWidget::log(const std::string& _msg, LogLevel _level, const std::string& _fileName, int _line)
 {
     // LEVEL column
     ui->tbLogConsole->insertRow(ui->tbLogConsole->rowCount());
@@ -26,11 +28,18 @@ void LogConsoleWidget::log(const std::string& _msg, LogLevel _level)
                               0,
                               item);
 
+    // PLACE column
+    item = new QTableWidgetItem(QString::fromStdString(_fileName + ":" + std::to_string(_line)));
+    item->setFlags(Qt::NoItemFlags);
+    ui->tbLogConsole->setItem(ui->tbLogConsole->rowCount() - 1,
+                              1,
+                              item);
+
     // MSG column
     item = new QTableWidgetItem(QString::fromStdString(_msg));
     item->setFlags(Qt::NoItemFlags);
     ui->tbLogConsole->setItem(ui->tbLogConsole->rowCount() - 1,
-                              1,
+                              2,
                               item);
 }
 
